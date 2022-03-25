ddbb2d66f943881ca310f79bf112197b4969ea17
UwUBeebi <chris.erman9@gmail.com>
2022-03-25 14:48:46 +0200

    As a user I want to be able to list an item [Delivers #181606398]

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());        // Avoid CORS errors in browsers
-app.use(express.json()) // Populate req.body
+app.use(cors());
+app.use(express.json())
 
 const items = [
-    { id: 1, name: "Tool", price: 6.99, desc: "Tammepuidust tool" },
-    { id: 2, name: "Kapp", price: 15.55, desc: "Vanamoodne kapp" },
-    { id: 3, name: "Laud", price: 59.99, desc: "Led-gamer desk" },
+    { id: 1, name: "Tool", price: "6.99", description: "Tammepuidust tool" },
+    { id: 2, name: "Kapp", price: "15.55", description: "Vanamoodne kapp" },
+    { id: 3, name: "Laud", price: "59.99", description: "Led-gamer desk" },
 ]
 
 app.get('/items', (req, res) => {
@@ -23,19 +23,17 @@ app.get('/items/:id', (req, res) => {
 })
 
 app.post('/items', (req, res) => {
-    if (!req.body.name || !req.body.price || !req.body.desc) {
+    if (!req.body.name || !req.body.price || !req.body.description) {
         return res.status(400).send({ error: 'One or all params are missing' })
     }
     let newItem = {
         id: items.length + 1,
         price: req.body.price,
         name: req.body.name,
-        desc: req.body.desc
+        description: req.body.description
     }
     items.push(newItem)
-    res.status(201).location('localhost:8080/items/' + (items.length)).send(
-        newItem
-    )
+    res.status(201).send(items)
 })
 
 app.listen(8080, () => {
--- rest-client.js
+++ rest-client.js
@@ -1,11 +1,12 @@
 const vue = Vue.createApp({
     data() {
         return {
-            itemInModal: {name: null},
-            items: []
+            itemInModal: { name: null },
+            items: [],
+            addModal: {}
         }
     },
-    async created(){
+    async created() {
         this.items = await (await fetch('http://localhost:8080/items')).json();
     },
     methods: {
@@ -13,6 +14,21 @@ const vue = Vue.createApp({
             this.itemInModal = await (await fetch(`http://localhost:8080/items/${id}`)).json()
             let itemInfoModal = new bootstrap.Modal(document.getElementById('itemInfoModal'), {})
             itemInfoModal.show()
+        },
+        addItem: async function () {
+            const newItem = {
+                name: this.addModal.name,
+                price: this.addModal.price,
+                description: this.addModal.description
+            }
+            console.log(newItem)
+            await fetch("http://localhost:8080/items", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newItem)
+            }).then(response => {this.items.push(newItem)})
         }
     }
 }).mount('#app')
